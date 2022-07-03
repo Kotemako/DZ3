2914b50a86a93065bde0f7889e13188ed4fa8684
Eugene Kotemako <kotemako@mail.ru>
2022-07-03 14:52:54 +0300

    Решение

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int threshold = 1000;
-        int refill;
-        refill = 1700;
-        int balance;
-        balance = 300;
-
-        int bonus;
-        if(refill > threshold) {
-            bonus = refill / 100;
-        } else {
-            bonus = 0;
-        }
-
-        int totalBalance;
-        totalBalance = balance + refill + bonus;
-
-        System.out.println(totalBalance);
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
